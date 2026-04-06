65818dc4f8fc7501d009fee502241bdcd1674556
medhkims <121733385+medhkims@users.noreply.github.com>
2026-04-07 00:34:04 +0200

    fix: use correct font-family 'material-community' in @font-face for web icons

--- app/+html.tsx
+++ app/+html.tsx
@@ -11,12 +11,11 @@ export default function Root({ children }: PropsWithChildren) {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        {/* Preload the icon font so glyphs are ready before React hydrates */}
         <link rel="preload" href={FONT_PATH} as="font" type="font/ttf" crossOrigin="anonymous" />
         <style dangerouslySetInnerHTML={{
           __html: `
             @font-face {
-              font-family: 'MaterialCommunityIcons';
+              font-family: 'material-community';
               src: url('${FONT_PATH}') format('truetype');
               font-display: block;
             }
