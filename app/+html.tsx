eddf84b1a0c4c9bb2e2069864731f18ea936a5ac
medhkims <121733385+medhkims@users.noreply.github.com>
2026-04-07 00:27:52 +0200

    fix: inject @font-face via +html.tsx style tag to fix missing icons on web

--- app/+html.tsx
+++ app/+html.tsx
@@ -1,6 +1,9 @@
 import { ScrollViewStyleReset } from 'expo-router/html';
 import type { PropsWithChildren } from 'react';
 
+const FONT_PATH =
+  '/assets/node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.6e435534bd35da5fef04168860a9b8fa.ttf';
+
 export default function Root({ children }: PropsWithChildren) {
   return (
     <html lang="en">
@@ -8,13 +11,17 @@ export default function Root({ children }: PropsWithChildren) {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link
-          rel="preload"
-          href="/assets/node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.6e435534bd35da5fef04168860a9b8fa.ttf"
-          as="font"
-          type="font/ttf"
-          crossOrigin="anonymous"
-        />
+        {/* Preload the icon font so glyphs are ready before React hydrates */}
+        <link rel="preload" href={FONT_PATH} as="font" type="font/ttf" crossOrigin="anonymous" />
+        <style dangerouslySetInnerHTML={{
+          __html: `
+            @font-face {
+              font-family: 'MaterialCommunityIcons';
+              src: url('${FONT_PATH}') format('truetype');
+              font-display: block;
+            }
+          `,
+        }} />
         <ScrollViewStyleReset />
       </head>
       <body>{children}</body>
