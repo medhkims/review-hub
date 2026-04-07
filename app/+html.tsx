5876970b2d6d73332c3efb20e34237bf20a12a62
medhkims <121733385+medhkims@users.noreply.github.com>
2026-04-07 17:34:00 +0200

    fix: serve icon font from /public/fonts to avoid node_modules path issue on Firebase

--- app/+html.tsx
+++ app/+html.tsx
@@ -1,8 +1,7 @@
 import { ScrollViewStyleReset } from 'expo-router/html';
 import type { PropsWithChildren } from 'react';
 
-const FONT_PATH =
-  '/assets/node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.6e435534bd35da5fef04168860a9b8fa.ttf';
+const FONT_PATH = '/fonts/MaterialCommunityIcons.ttf';
 
 export default function Root({ children }: PropsWithChildren) {
   return (
@@ -11,31 +10,24 @@ export default function Root({ children }: PropsWithChildren) {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
-        {/* Force-load the icon font synchronously before React hydrates */}
+        <link rel="preload" href={FONT_PATH} as="font" type="font/ttf" crossOrigin="anonymous" />
         <style dangerouslySetInnerHTML={{
           __html: `
             @font-face {
               font-family: 'material-community';
               src: url('${FONT_PATH}') format('truetype');
               font-display: block;
-              unicode-range: U+0020-FFFF;
             }
           `,
         }} />
-
-        {/* Use JS FontFace API for reliable cross-browser loading */}
         <script dangerouslySetInnerHTML={{
           __html: `
             (function() {
               var font = new FontFace('material-community', 'url(${FONT_PATH})', { display: 'block' });
-              font.load().then(function(loadedFont) {
-                document.fonts.add(loadedFont);
-              });
+              font.load().then(function(f) { document.fonts.add(f); });
             })();
           `,
         }} />
-
         <ScrollViewStyleReset />
       </head>
       <body>{children}</body>
