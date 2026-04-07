1913649e329e0e6961eab1773b7cfecb64fdf093
medhkims <121733385+medhkims@users.noreply.github.com>
2026-04-07 07:02:35 +0200

    fix: use FontFace JS API to force-load icon font before React hydrates

--- app/+html.tsx
+++ app/+html.tsx
@@ -11,16 +11,31 @@ export default function Root({ children }: PropsWithChildren) {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <link rel="preload" href={FONT_PATH} as="font" type="font/ttf" crossOrigin="anonymous" />
+
+        {/* Force-load the icon font synchronously before React hydrates */}
         <style dangerouslySetInnerHTML={{
           __html: `
             @font-face {
               font-family: 'material-community';
               src: url('${FONT_PATH}') format('truetype');
               font-display: block;
+              unicode-range: U+0020-FFFF;
             }
           `,
         }} />
+
+        {/* Use JS FontFace API for reliable cross-browser loading */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function() {
+              var font = new FontFace('material-community', 'url(${FONT_PATH})', { display: 'block' });
+              font.load().then(function(loadedFont) {
+                document.fonts.add(loadedFont);
+              });
+            })();
+          `,
+        }} />
+
         <ScrollViewStyleReset />
       </head>
       <body>{children}</body>
